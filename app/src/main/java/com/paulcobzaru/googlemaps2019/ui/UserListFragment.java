a952508845d7cdf9ba257a9b78c6da79f6701b49
Paul <paulcobzaru0@gmail.com>
2019-09-06 18:30:42 +0300

    Displaying a Google Map using a MapView

--- app/src/main/java/com/paulcobzaru/googlemaps2019/ui/UserListFragment.java
+++ app/src/main/java/com/paulcobzaru/googlemaps2019/ui/UserListFragment.java
@@ -12,19 +12,26 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.paulcobzaru.googlemaps2019.R;
 import com.paulcobzaru.googlemaps2019.adapters.UserRecyclerAdapter;
 import com.paulcobzaru.googlemaps2019.models.User;
 
 import java.util.ArrayList;
 
-public class UserListFragment extends Fragment {
+import static com.paulcobzaru.googlemaps2019.Constants.MAPVIEW_BUNDLE_KEY;
+
+public class UserListFragment extends Fragment implements OnMapReadyCallback {
 
     private static final String TAG = "UserListFragment";
 
     //widgets
     private RecyclerView mUserListRecyclerView;
-
+    private MapView mMapView;
 
     //vars
     private ArrayList<User> mUserList = new ArrayList<>();
@@ -49,8 +56,26 @@ public class UserListFragment extends Fragment {
         View view  = inflater.inflate(R.layout.fragment_user_list, container, false);
         mUserListRecyclerView = view.findViewById(R.id.user_list_recycler_view);
 
+        mMapView = (MapView) view.findViewById(R.id.user_list_map);
+
         initUserListRecyclerView();
+
+        initGoogleMap(savedInstanceState);
+
         return view;
+    }
+
+    private void initGoogleMap(Bundle savedInstanceState) {
+        // *** IMPORTANT ***
+        // MapView requires that the Bundle you pass contain _ONLY_ MapView SDK
+        // objects or sub-Bundles.
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
+        }
+        mMapView.onCreate(mapViewBundle);
+
+        mMapView.getMapAsync(this);
     }
 
 
@@ -58,6 +83,61 @@ public class UserListFragment extends Fragment {
         mUserRecyclerAdapter = new UserRecyclerAdapter(mUserList);
         mUserListRecyclerView.setAdapter(mUserRecyclerAdapter);
         mUserListRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Bundle mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY);
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle);
+        }
+
+        mMapView.onSaveInstanceState(mapViewBundle);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mMapView.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mMapView.onStop();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        map.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        map.setMyLocationEnabled(true);
+    }
+
+    @Override
+    public void onPause() {
+        mMapView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        mMapView.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
     }
 }
 
